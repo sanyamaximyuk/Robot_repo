1d8e80210d57d08cbdb313e026919d123499044e
sanyamaximyuk <85547224+sanyamaximyuk@users.noreply.github.com>
2025-01-31 14:21:34 +0200

    arifm

---
+++ Homeworks/js-eslint-config/HW3/arifmetic.js
@@ -0,0 +1,21 @@
+const a = 5;
+let b = '7';
+b++;
+c = -3.23;
+const d = true;
+let e = 'string';
+const f = undefined;
+const g = {
+    key1: 4,
+    key2: 'key'
+};
+
+console.log(a,b);
+console.log(a-b*c);
+console.log(typeof a-b);
+console.log(g);
+console.log(b**c);
+console.log(b);
+
+
+
